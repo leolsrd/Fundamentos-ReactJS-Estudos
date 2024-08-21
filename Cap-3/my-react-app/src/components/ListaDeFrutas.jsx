1c0fccc8da1c7306f759a519caf9f9a06332a7da
Léo Lourenço <leo.lsrd@gmail.com>
2024-08-20 23:52:37 -0300

    feat: Componente ListaDeFrutas cap-3

---
+++ Cap-3/my-react-app/src/components/ListaDeFrutas.jsx
@@ -0,0 +1,18 @@
+/* eslint-disable react/prop-types */
+
+function ListaDeFrutas(props) {
+  return (
+    <div>
+      <ul>
+        {props.frutas.map((fruta) => (
+          <li key={fruta}>{fruta}</li>
+        ))}
+      </ul>
+      <p>
+        {props.pessoa.nome} tem {props.pessoa.idade} anos
+      </p>
+    </div>
+  );
+}
+
+export default ListaDeFrutas;
