a21f7260d41b2dfd8be9d54ec82a7cccf9b333c1
Léo Lourenço <leo.lsrd@gmail.com>
2024-08-23 23:52:39 -0300

    feat: Estudando sobre useState

---
+++ Cap-5/my-react-app/src/components/Contador.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+function Contador() {
+  const [contagem, setContagem] = useState(0);
+
+  const incrementar = () => {
+    setContagem(contagem + 1);
+  };
+
+  return (
+    <div>
+      <p>Contagem: {contagem}</p>
+      <button onClick={incrementar}>Incrementar</button>
+    </div>
+  );
+}
+
+export default Contador;
