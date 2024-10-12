f85b7bb05aeb8e7a96df4209975966e3ded6f5d7
Léo Lourenço <leo.lsrd@gmail.com>
2024-10-11 23:34:27 -0300

    feat: Nas rotas estou seguindo a documentação pois está mais atual que o livro

--- Cap-8/my-react-app/src/main.jsx
+++ Cap-8/my-react-app/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { StrictMode } from "react";
+import App from "./App.jsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import * as ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./components/Home.jsx";
+import AppDefault from "./components/AppDefault.jsx";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <BrowserRouter>
+    <StrictMode>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="home" element={<Home />} />
+        <Route path="AppDefault" element={<AppDefault />} />
+      </Routes>
+    </StrictMode>
+  </BrowserRouter>
+);
