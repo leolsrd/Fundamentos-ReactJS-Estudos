56a52d1fd77e709382112d9b0b9c0c30b01a6ed5
Léo Lourenço <leo.lsrd@gmail.com>
2024-10-21 21:47:37 -0300

    feat: Continuando os estudos do capítulo 8 (react router)

--- Cap-8/my-react-app/src/routes/ListaDeUsuarios.jsx
+++ Cap-8/my-react-app/src/routes/ListaDeUsuarios.jsx
@@ -1,17 +1,25 @@
-import { useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 
 function ListaDeUsuarios() {
   const location = useLocation();
+  const navigate = useNavigate();
+
   const queryParams = new URLSearchParams(location.search);
 
   const pagina = queryParams.get("pagina") || "1";
-  const ordem = queryParams.get("ordem") || "ascendente";
+  // const ordem = queryParams.get("ordem") || "ascendente";
+
+  const irParaProximaPagina = () => {
+    queryParams.set("pagina", (parseInt(pagina) + 1).toString());
+    // queryParams.get("pagina");
+    navigate(location.pathname + "?" + queryParams.toString());
+  };
 
   return (
     <div>
       <h1>Lista de Uusários</h1>
       <p>Página Atual: {pagina}</p>
-      <p>Ordem: {ordem}</p>
+      <button onClick={irParaProximaPagina}>Próxima página</button>
     </div>
   );
 }
