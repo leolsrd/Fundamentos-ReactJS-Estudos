0e9633e4477dbf378dfd19d74e8bcbe49d824e41
Léo Lourenço <leo.lsrd@gmail.com>
2024-11-12 20:48:56 -0300

    feat: Partir para estilização

--- lista-de-tarefas/meu-app-tarefas/src/App.jsx
+++ lista-de-tarefas/meu-app-tarefas/src/App.jsx
@@ -1,23 +1,22 @@
 // import './App.css'
 
+import Header from "./components/Header";
+import TaskInput from "./components/TaskInput";
+import TaskList from "./components/TaskList";
+import TaskCounter from "./components/TaskCounter";
+import useStateWithLocalStorage from "./hooks/useStateWithLocalStorage";
 import { useState } from "react";
-import Header from "./assets/components/Header";
-import TaskInput from "./assets/components/TaskInput";
-import TaskList from "./assets/components/TaskList";
-import TaskCounter from "./assets/components/TaskCounter";
 
 function App() {
-  const [tasks, setTasks] = useState("todos", []);
+  const [tasks, setTasks] = useStateWithLocalStorage("todos", []);
+  const [filter, setFilter] = useState("all");
 
   const handleAddTask = (text) => {
     setTasks([...tasks, { id: Date.now(), text, completed: false }]);
-
-    tasks.length
-      ? localStorage.setItem("tasksSaved", JSON.stringify(tasks))
-      : null;
   };
 
   const handleToggleTask = (id) => {
+    console.log("Chegou aqui");
     setTasks(
       tasks.map((task) =>
         task.id === id ? { ...task, completed: !task.completed } : task
@@ -29,13 +28,25 @@ function App() {
     setTasks(tasks.filter((task) => task.id !== id));
   };
 
+  const filteredTasks = tasks.filter((task) => {
+    if (filter === "all") return true;
+    if (filter === "completed") return task.completed;
+    if (filter === "active") return !task.completed;
+    return false;
+  });
+
   return (
     <>
       <div className="app">
         <Header />
         <TaskInput onAddTask={handleAddTask} />
+        <div>
+          <button onClick={() => setFilter("all")}>Todas</button>
+          <button onClick={() => setFilter("completed")}>Completas</button>
+          <button onClick={() => setFilter("active")}>Ativas</button>
+        </div>
         <TaskList
-          tasks={tasks}
+          tasks={filteredTasks}
           onToggle={handleToggleTask}
           onDelete={handleDeleteTask}
         />
